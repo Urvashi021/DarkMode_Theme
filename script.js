270c725f52747e59bf0a50794c58d572121f46f5
Urvashi021 <127461389+Urvashi021@users.noreply.github.com>
2023-05-20 20:46:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// using with condition 
+
+// let theme = 'light';
+// const toggleTheme = () => {
+//     theme = theme === 'light'? 'dark' : 'light';
+
+//     if (theme === 'light') {
+//         document.body.style.backgroundColor = 'white';
+//         document.body.style.color = 'black';
+//     } else {
+//         document.body.style.backgroundColor = 'black';
+//         document.body.style.color = 'white';
+//     }
+// }
+
+
+// using with event
+
+// const button= document.getElementById('btn');
+// button.addEventListener('click', function(){
+//     document.body.classList.toggle('dark')
+// })
